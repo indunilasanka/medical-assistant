471d2eae41e8218a0c9c140592c7cd1f1faa1c50
indunilasanka <waiasanka.13@cse.mrt.ac.lk>
2018-06-29 23:48:01 +0530

    delete unwanted packages

--- Template/platforms/ios/www/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-﻿myApp.controller('MenuCtrl', function ($scope) {
-    $scope.$on("profile", function (event, args) {
-        $scope.profileName = args.FirstName + " " +args.LastName;
-        $scope.companyName = "@ " + args.CompanyName;
-    });
-});
